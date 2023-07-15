2e3753803ddbf8887bc53857deff48e5815a616a
Drozerah <drozerah@gmail.com>
2023-07-15 11:06:12 +0200

    refactor(./src): refactor scss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './style.scss'
+import './scss/style.scss'
 import { AutoHeightCarousel as myCarousel } from '../lib/AutoHeightCarousel.js'
 
 let carousel = document.querySelector('#carousel') as HTMLElement
